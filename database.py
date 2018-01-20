56f616086c2c8206cb0578c3408b0ae0f29c0096
elaieck <35596572+elaieck@users.noreply.github.com>
2018-01-20 23:17:34 +0000

    database class and server build

---
+++ database.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+
+def open_db():
+    pass
+
+def close_db():
+    pass
+
+def commit():
+    pass
+
+def get_users():
+    pass
+
+def get_user():
+    pass
+
+def get_games():
+    pass
+
+def get_game():
+    pass
+
+def get_game_posts():
+    pass
+
+def create_new_user():
+    pass
+
+def create_new_post():
+    pass
+
+def bind_friends():
+    pass
+
+def detach_friends():
+    pass
+
+def add_user_game():
+    pass
+
--- server.py
+++ server.py
@@ -1,16 +1,20 @@
 import socket
 import threading
+import database
+import json
 
-sock = socket.socket()
+srv_sock = socket.socket()
 ip = "0.0.0.0" # means local
 port = 60000
-sock.bind((ip, port))
-sock.listen(10)
+srv_sock.bind((ip, port))
+srv_sock.listen(10)
 threads = []
 
-def server(new_sock):
-    # get_authen()
-    # send_menu()
+def server(sock):
+    while True:
+        username = get_authen()
+        if username is not None:
+            break
     # game = get_choice()
     # send_game_info(game)
     # send_posts(game)
@@ -18,8 +22,48 @@ def server(new_sock):
         data = new_sock.recv(1024)
 
 while True:
-    cli_s, addr = sock.accept()
+    cli_s, addr = srv_sock.accept()
     t = threading.Thread(target=server, args=(cli_s,))
     print "hi"
     t.start()
     threads.append(t)
+
+def parts(message):
+    return message.split("~")
+
+
+def get_authen(sock):
+    data = sock.recv(1024)
+    message = parts(data)
+    if message[0] == "LOGIN":
+        user = database.get_user(message[1])
+        if user is None:
+            sock.send("LOF")
+            return ""
+        elif user.password != message[2]:
+            sock.send("LOF")
+            return ""
+        else:
+            sock.send("LOS")
+            return user
+    else:
+        if database.get_user(message[1]) is not None:
+            sock.send("SUF")
+            return ""
+        else:
+            database.create_new_user(message[1], message[2])
+            return ""
+
+
+def recv_choice(sock):
+    game = sock.recv(1024)
+    game = database.get_game(game)
+    sock.send("GINFO~" + game.price + "~" + game.image_base64)
+    posts = database.get_game_posts(game)
+    string_posts = []
+    for post in posts:
+        string_posts.append(json.dumps(post))
+    sock.send("GINFO~" + "~".join(posts))
+
+
+
