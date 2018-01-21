22f21e2a171f42180e366e19a67f9c1141022701
elaieck <35596572+elaieck@users.noreply.github.com>
2018-01-21 22:50:36 +0000

    add graphics to client

--- client.py
+++ client.py
@@ -1,11 +1,65 @@
 import socket
+import pygame
+import graphics
 import threading
 
-sock = socket.socket()
-ip = "127.0.0.1"    # means local
-port = 60000
-sock.connect((ip, port))
-i = 0
+
+pygame.init()
+screen = pygame.display.set_mode((750, 538))
+clock = pygame.time.Clock()
+
+# sock = socket.socket()
+# ip = "127.0.0.1"    # means local
+# port = 60000
+# sock.connect((ip, port))
+
+#authentication screen
+login_background = pygame.image.load("authen.jpg")
+login_username_box = graphics.text_box(screen, 200, 175, 350, 40, "username")
+login_password_box = graphics.text_box(screen, 200, 241, 350, 40, "password")
+Login_button = graphics.button(195, 307, 360, 43, "sign in")
+create_account_button = graphics.button(394, 371, 151, 20, "create account")
+
+#register screen
+signup_background = pygame.image.load("signup.jpg")
+signup_username_box = graphics.text_box(screen, 200, 170, 350, 40, "username")
+signup_password_box = graphics.text_box(screen, 200, 236, 350, 40, "password")
+signup_confirm_box = graphics.text_box(screen, 200, 300, 350, 40, "confirm password")
+sign_up_button = graphics.button(196, 366, 360, 43, "sign up")
+
+
+current_screen = "login"
 while True:
-    sock.send(str(i))
-    i+=1
+
+    while current_screen == "login":
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                exit()
+
+        screen.blit(login_background, (0, 0))
+        login_username_box.update(events)
+        login_password_box.update(events)
+
+        if create_account_button.is_pressed():
+            current_screen = "signup"
+
+        pygame.display.flip()
+
+    while current_screen == "signup":
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                exit()
+
+        screen.blit(signup_background, (0, 0))
+        signup_username_box.update(events)
+        signup_password_box.update(events)
+        signup_confirm_box.update(events)
+
+        if sign_up_button.is_pressed():
+            current_screen = "login"
+
+        pygame.display.flip()
+
+
--- graphics.py
+++ graphics.py
@@ -3,7 +3,8 @@ import pygame_textinput
 
 class text_box():
 
-    def __init__(self, x , y, width, height, text="", size=30):
+    def __init__(self, screen, x , y, width, height, text="", size=30):
+        self.screen = screen
         self.x = x
         self.y = y
         self.width = width
@@ -28,19 +29,19 @@ class text_box():
             else:
                 self.activated = False
 
-    def update(self):
+    def update(self, events):
         self.update_press()
 
         if self.activated:
             self.textinput.update(events)
-            screen.blit(self.textinput.get_surface(), (self.x+4, self.y+11))
+            self.screen.blit(self.textinput.get_surface(), (self.x+4, self.y+11))
         else:
             font = pygame.font.SysFont('', self.size)
             if self.textinput.get_text() == "":
                 text = font.render(self.text, True, (127, 127, 127))
             else:
                 text = font.render(self.textinput.get_text(), True, (0, 0, 0))
-            screen.blit(text, (self.x+4, self.y+11))
+            self.screen.blit(text, (self.x+4, self.y+11))
 
 class button():
 
@@ -55,24 +56,25 @@ class button():
 
     def _is_in(self):
         x_mouse, y_mouse = pygame.mouse.get_pos()
-        asd = self.x<x_mouse<self.x+self.width and self.y<y_mouse<self.y+self.height
-        return asd
+        return self.x<x_mouse<self.x+self.width and self.y<y_mouse<self.y+self.height
 
-    def get_press(self):
+
+    def is_pressed(self):
         pressed1, pressed2, pressed3 = pygame.mouse.get_pressed()
         if pressed1 and self._is_in():
             return True
         return False
 
 class image_button(button):
-    def __init__(self, x, y, image):
+    def __init__(self,screen, x, y, image):
         self.image = pygame.image.load(image)
         button.__init__(self, x, y, self.image.get_width, self.image.get_height)
         self.surface = pygame.Surface((1, 1))
         self.surface.set_alpha(0)
+        self.screen = screen
 
     def show(self):
-        screen.blit(self.image, (self.x, self.y))
+        self.screen.blit(self.image, (self.x, self.y))
 
 
 
@@ -86,34 +88,36 @@ class image_button(button):
 
 
 
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((750, 538))
+    clock = pygame.time.Clock()
 
-pygame.init()
-screen = pygame.display.set_mode((750, 538))
-clock = pygame.time.Clock()
+    username_box = text_box(screen, 200, 175, 350, 40, "username")
+    password_box = text_box(screen, 200, 241, 350, 40, "password")
 
-username_box = text_box(200, 175, 350, 40, "username")
-password_box = text_box(200, 241, 350, 40, "password")
+    while True:
+        screen.fill((225, 225, 225))
 
-while True:
-    screen.fill((225, 225, 225))
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                exit()
 
-    events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.QUIT:
-            exit()
+        background = pygame.image.load("authen.jpg")
+        screen.blit(background, (0, 0))
 
-    pos = pygame.mouse.get_pos()
-    font = pygame.font.SysFont('arial', 30)
-    text = font.render(str(pos), True, (0, 0, 0))
+        pos = pygame.mouse.get_pos()
+        font = pygame.font.SysFont('arial', 30)
+        text = font.render(str(pos), True, (0, 0, 0))
+        pygame.draw.rect(screen, (255, 255, 255), (0, 0, 150, 50))
+        screen.blit(text, (2, 2))
 
+        username_box.update(events)
+        password_box.update(events)
 
-    background = pygame.image.load("authen.jpg")
-    screen.blit(background, (0, 0))
-    pygame.draw.rect(screen, (255, 255, 255), (0, 0, 150, 50))
-    screen.blit(text, (2, 2))
+        pygame.display.update()
+        clock.tick(30)
 
-    username_box.update()
-    password_box.update()
-
-    pygame.display.update()
-    clock.tick(30)
+if __name__ == '__main__':
+    main()
